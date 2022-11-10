9943fab0ed8971e9dd7ff9f724934077bae4604a
Tim Norris <tim.norris@smartsurjoutlook.onmicrosoft.com>
2022-11-10 16:33:27 -0500

    Added Solution and RomanNumerals Projects

---
+++ Katas.RomanNumerals.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,10 @@
+namespace Katas.RomanNumerals.Tests;
+
+public class RomanNumeralsTests
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
---
+++ Katas.RomanNumerals/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Katas.RomanNumerals;
+public class Class1
+{
+
+}
